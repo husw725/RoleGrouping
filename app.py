d7d1e73a159ef443a1bc305be36ec7e40eea75a5
husw <siwen.hu@melot.cn>
2025-11-18 20:52:40 +0800

    optimize code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from step3_prompt_check import run_step3
 def main():
     # Page configuration
     st.set_page_config(
-        page_title="Character Grouping Tool",
+        page_title="Character Grouping Tool(v251118)",
         layout="wide",
         page_icon="🧩",
         initial_sidebar_state="expanded"
